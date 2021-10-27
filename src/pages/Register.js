df38ac02ebb4d07cdef81cfcd9c5ff2779f98b04
amarealcoder <ugorjimiracle06@gmail.com>
2021-10-27 14:31:55 +0100

    added date validation on date input

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,32 +2,35 @@ import './Register.css';
 import { Container, TextField, Typography, Button } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import React, { useState } from 'react';
-
-// const useStyles = makeStyles({
-//   fields: {
-//     display: 'block',
-//   },
-// });
-// const handleChange = () => {
-//   e.preventDefault();
-//   // {e.target.value} =
-// };
+import validator from 'validator';
 
 const Register = () => {
   const [name, setName] = useState('');
   const [date, setDate] = useState('');
   const [slogan, setSlogan] = useState('');
 
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const validateDate = (value) => {
+    if (validator.isDate(value)) {
+      setErrorMessage('Valid date');
+      setDate(value);
+    } else {
+      setErrorMessage('Enter valid date!: year/month/day');
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name && date && slogan) {
+      // validateDate();
       console.log(name, date, slogan);
     }
   };
 
   return (
     <Container>
-      <Typography variant='h4' color='secondary' align='center'>
+      <Typography variant='h4' mt={3} color='secondary' align='center'>
         Register Your Party
       </Typography>
       <form
@@ -48,16 +51,19 @@ const Register = () => {
           }}
         />
         <TextField
-          onChange={(e) => setDate(e.target.value)}
+          onChange={(e) => validateDate(e.target.value)}
+          
           className='field'
           id='outlined-basic'
-          label='Date joined'
+          label='Date joined: yyyy/mm/dd'
           variant='outlined'
           color='secondary'
           sx={{
             marginTop: 2,
           }}
         />
+        <span>{errorMessage}</span>
+
         <TextField
           onChange={(e) => setSlogan(e.target.value)}
           className='field'
--- src/pages/Vote.js
+++ src/pages/Vote.js
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 
-const Vote = () => {
-  const [parties, setParties] = useState([]);
+// const Vote = () => {
+//   const [parties, setParties] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:8000/parties')
-      .then((res) => res.json())
-      .then((data) => setParties(data));
-  }, []);
-  return (
-    <div className='App'>
-      {parties.map((party) => (
-        <h3>{party.name}</h3>
-      ))}
-    </div>
-  );
-};
+//   useEffect(() => {
+//     fetch('http://localhost:8000/parties')
+//       .then((res) => res.json())
+//       .then((data) => setParties(data));
+//   }, []);
+//   return (
+//     <div className='App'>
+//       {parties.map((party) => (
+//         <h3>{party.name}</h3>
+//       ))}
+//     </div>
+//   );
+// };
 
-export default Vote;
+// export default Vote;
